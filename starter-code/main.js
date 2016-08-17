884bbcfffef0d6dd0548c5b099960cb84b1eaf02
Ramata Diallo <Ramata Diallo>
2016-08-16 22:21:46 -0400

    added started-code

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,27 @@
-console.log("JS file is connected to HTML! Woo!")
+
+var cardOne= "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardOne === cardFour) {
+	alert('You found a match!');
+} else {
+	      alert('Sorry, try again.');
+}	     
+if (cardOne === cardTwo) {
+	     alert('You found a match!');
+} else {
+	      alert('Sorry, try again.');
+}	      
+
+if ( cardFour === cardThree) {
+          alert('You found a match!');    
+ } else {
+	      alert('Sorry, try again.');
+}	       
+if ( cardTwo === cardThree) {
+ 	       alert('You found a match!');
+} else {
+	      alert('Sorry, try again.');
+}	      
