3c5da0d120e6d34104d7e1eff96cd60ae9a5b7da
Ramata Diallo <Ramata Diallo>
2016-08-20 23:36:18 -0400

    created a Board with JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,11 @@
 
+
 var cardOne= "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardFour) {
+/*if (cardOne === cardFour) {
 	alert('You found a match!');
 } else {
 	      alert('Sorry, try again.');
@@ -24,4 +25,14 @@ if ( cardTwo === cardThree) {
  	       alert('You found a match!');
 } else {
 	      alert('Sorry, try again.');
-}	      
+}*/
+var createBoard = function(){
+	for (var i= 0; i<4; i++){
+		var card = document.createElement('div');
+		card.className = 'card';
+		document.getElementsByClassName('board')[0].appendChild(card);
+		}	
+}
+
+var gameBoard = document.getElementById('game-board'); 
+createBoard();
